bfa9c2dfaf27d8346fd4fb5c0ac3fa215236fb5a
charlie763 <cwisoff@gmail.com>
2020-05-29 12:09:06 -0600

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+	name_keys_array = name_hash.map{|key, value| [key, value]}
+	name_keys_array.inject{|memo, pair| memo[1]<pair[1] ? memo : pair}[0] if name_hash.length > 0
 end
