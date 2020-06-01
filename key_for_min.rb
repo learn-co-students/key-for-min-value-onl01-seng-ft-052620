ae6344d90e1262015a645613d17960089b6c475e
Nroulston <nicolas.roulston@gmail.com>
2020-05-31 23:29:25 -0700

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,13 +6,22 @@ require 'pry'
 def key_for_min_value(name_hash)
   return nil if name_hash.empty?
   small_value = nil
-  small_value_key = 0
-    name_hash.each do |key, value|
+
+    name_hash.each_value do |value|
       if small_value.nil?
         small_value = value
-        small_value_key = key
       end
-      small_value_key, small_value = key, value if (small_value <=> value) == 1
+       small_value = value if (small_value <=> value) == 1
     end
+    
    return name_hash.rassoc(small_value)[0]    
 end
+
+=begin 
+Could you have used a method other than rassoc? maybe name_hash.select {|k,v| v = small_value}  
+it returns an array #=> {:adam => 1}.to_a to make it an nested array [[:adam, 1]] then accessed the
+array that way.
+
+Maybe you could have broken it into an array in the first place.
+
+=end
